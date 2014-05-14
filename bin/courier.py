060cc53525ea7caf4bd1f9e0d0d6693b0a3ce3f3
shearn89 <alexander.shearn@gmail.com>
2014-05-14 23:48:12 +0100

    working nicely with SSH and config file! Will need to begin error handling at some point.

--- bin/courier.py
+++ bin/courier.py
@@ -3,7 +3,6 @@
 ## notes:
 ## marshall data to /var/lib/courier/
 ## TODO: service script
-## TODO: SSH
 
 import time
 import signal
@@ -18,9 +17,9 @@ from ConfigParser import SafeConfigParser
 
 ## Variables
 config = SafeConfigParser()
-config.read('etc/courier.ini')
+config.read(['etc/courier.ini','/etc/courier/courier.ini'])
 
-watchFolder = config.get("Courier", "watchFolder")
+watchFolder = os.path.expanduser(os.path.expandvars(config.get("Courier", "watchFolder")))
 targetFolder = config.get("Courier", "targetFolder")
 remoteFolder = config.get("Courier", "remoteFolder")
 fileExtension = config.get("Courier", "fileExtension")
@@ -28,11 +27,11 @@ targetIP = config.get("Courier", "targetIP")
 move = config.getboolean("Courier", "move")
 flatten = config.getboolean("Courier", "flatten")
 
-verb="processing"
+verb="Processing"
 if move:
-	verb="moving"
+	verb="Moving"
 else:
-	verb="copying"
+	verb="Copying"
 
 print "Watching:    %s" % watchFolder
 print "Target:      %s" % targetFolder
@@ -62,10 +61,8 @@ def interrupt_handler(signal, frame):
 
 def hup_handler(signal, frame):
 	global copiedList
-	print "SIGHUP caught, %s all files." % verb
+	print "SIGHUP caught. %s all files still in %s" % (verb, watchFolder)
 	copiedList = copiedList - fileList
-	print copiedList
-	print fileList
 
 signal.signal(signal.SIGHUP, hup_handler) # 1
 signal.signal(signal.SIGINT, interrupt_handler) # 2
@@ -75,15 +72,13 @@ signal.signal(signal.SIGTERM, interrupt_handler) # 15
 ###################
 ## Main
 def push_file(filename):
-	print "\npushing %s to %s" % (filename, targetFolder)
+	print "%s %s to %s" % (verb, filename, targetFolder)
 	file='/'.join(filename.split('/')[1:])
-	print "file: %s" % file
 	targetFile=""
 	remoteFile=os.path.join(remoteFolder,os.path.split(filename)[1])
+	remoteFile=os.path.expandvars(os.path.expanduser(remoteFile))
 	if flatten:
-		print "flatten was true!"
 		targetFile=os.path.join(targetFolder,os.path.split(filename)[1])
-		print targetFile
 	else:
 		subpath=os.path.split(file)[0]
 		targetPath=os.path.join(targetFolder,subpath)
@@ -94,38 +89,42 @@ def push_file(filename):
 		except OSError:
 			if not os.path.isdir(targetPath):
 				raise
-	print targetFile
 	if move:
-		print "moving!"
-		print filename
-		print targetFile
 		shutil.move(filename,targetFile)
 	else:
 		shutil.copy(filename,targetFile)
-	scp.put(filename,remoteFile)
+	if actualssh:
+		print "Sending %s to %s" % (filename, remoteFile)
+		scp.put(filename,remoteFile)
 	copiedList.add(filename)
 
-ssh = paramiko.SSHClient()
-ssh.load_system_host_keys()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-try:
-	ssh.connect(targetIP, timeout=5)
-except paramiko.ssh_exception.AuthenticationException:
-	print "Authentication failed, please fix manually."
-	sys.exit(1)
-except socket.gaierror:
-	print "Address info not valid, please check config file."
-	sys.exit(1)
-except socket.timeout:
-	print "SSH Connection timed out, please check host %s is up." % targetIP
-	sys.exit(1)
+## Initialise SSH conection
+actualssh=True
+if ( targetIP == "127.0.0.1" or targetIP == "localhost" ):
+	actualssh=False
 
-scp = scp.SCPClient(ssh.get_transport())
+if actualssh:
+	ssh = paramiko.SSHClient()
+	ssh.load_system_host_keys()
+	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	try:
+		ssh.connect(targetIP, timeout=5)
+	except paramiko.ssh_exception.AuthenticationException:
+		print "Authentication failed, please fix manually."
+		sys.exit(1)
+	except socket.gaierror:
+		print "Address info not valid, please check config file."
+		sys.exit(1)
+	except socket.timeout:
+		print "SSH Connection timed out, please check host %s is up." % targetIP
+		sys.exit(1)
+	scp = scp.SCPClient(ssh.get_transport())
 
+print "Beginning monitoring..."
 while True:
 	time.sleep(1)
-	sys.stdout.write(".")
-	sys.stdout.flush()
+	# sys.stdout.write(".")
+	# sys.stdout.flush()
 	for root, dirs, files in os.walk(watchFolder):
 		for file in files:
 			if file.endswith(fileExtension):
