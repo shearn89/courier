a3dccbecf4a6881b64bc4bf8f77880ad2844ea3b
shearn89 <alexander.shearn@gmail.com>
2014-05-14 22:06:42 +0100

    working nicely. Need to add SSH

--- courier.py
+++ courier.py
@@ -8,25 +8,33 @@ import time
 import signal
 import sys
 import os
+import shutil
 import cPickle as pickle
 
 ## Variables
 watchFolder="data"
 targetFolder="processed"
 fileExtension=".txt"
+move=False
+flatten=True
 
-print watchFolder
-print targetFolder
-print fileExtension
+print "Watching:    %s" % watchFolder
+print "Target:      %s" % targetFolder
+print "Extension:   %s" % fileExtension
+print "Moving?      %s" % move
+print "Flatten?     %s" % flatten
 
 ## Data storage
 fileList = set()
 
+## Initialise copied list
 try:
 	copiedList = pickle.load(open("copied_files.pkl", "rb"))
 except IOError:
 	print "Database not found, creating new one."
 	copiedList = set()
+
+print "Copied files: %s " % copiedList
 
 ## Signal handling
 def signal_handler(signal, frame):
@@ -36,20 +44,37 @@ def signal_handler(signal, frame):
 
 signal.signal(signal.SIGINT, signal_handler)
 
+###################
 ## Main
 def push_file(filename):
-	print "pushing %s to %s" % (filename, targetFolder)
+	print "\npushing %s to %s" % (filename, targetFolder)
+	file='/'.join(filename.split('/')[1:])
+	print "file: %s" % file
+	subpath=os.path.split(file)[0]
+	targetPath=os.path.join(targetFolder,subpath)
+	print os.path.split(targetPath)[0]
+	targetFile=os.path.join(targetFolder,file)
+	print "target: %s" % targetFile
+	try:
+		print "Creating directory %s " % targetPath
+		os.makedirs(targetPath)
+	except OSError:
+		if not os.path.isdir(targetPath):
+			raise
+	if move:
+		shutil.move(filename,targetFile)
+	else:
+		shutil.copy(filename,targetFile)
 	copiedList.add(filename)
 
 while True:
 	time.sleep(1)
+	sys.stdout.write(".")
+	sys.stdout.flush()
 	for root, dirs, files in os.walk(watchFolder):
 		for file in files:
 			if file.endswith(fileExtension):
 				fn=os.path.join(root, file)
 				fileList.add(fn)
-				print "to copy: "
-				print (fileList - copiedList)
 				if fn not in copiedList:
 					push_file(fn)
-				print "copied: ", copiedList
